0aa4d9f9bc0f4855468a99d7f5b567b40db13865
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-04-09 15:23:06 +1200

    Initial ash resistance stat + genes

---
+++ 1.4/Source/MorrowRim2/MorrowRim2/DefOf/StatDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+
+namespace MorrowRim2
+{
+    [DefOf]
+    public static class StatDefOf
+    {
+        static StatDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(StatDefOf));
+        }
+        public static StatDef MorrowRim_AshResistance;
+    }
+}
