3e0b510e3d0bc7639059159440f0ed13487bf555
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-04-13 10:35:21 +1200

    custom basalt terrain

--- 1.4/Source/MorrowRim2/MorrowRim2/DefModExtension/RockProperties.cs
+++ 1.4/Source/MorrowRim2/MorrowRim2/DefModExtension/RockProperties.cs
@@ -5,6 +5,10 @@ namespace MorrowRim2
     public class RockProperties : DefModExtension
     {
         public bool onlyAllowIfForced = false;
+        public string roughTexPathReplacer;
+        public string roughHewnTexPathReplacer;
+        public string smoothTexPathReplacer;
+        public string scatterTypeReplacer = null;
 
         public static RockProperties Get(Def def)
         {
--- 1.4/Source/MorrowRim2/MorrowRim2/Utility/OnStartupUtility.cs
+++ 1.4/Source/MorrowRim2/MorrowRim2/Utility/OnStartupUtility.cs
@@ -1,6 +1,7 @@
 ﻿using Verse;
 using System.Collections.Generic;
 using RimWorld;
+using System.Linq;
 
 namespace MorrowRim2
 {
@@ -14,6 +15,7 @@ namespace MorrowRim2
         static OnStartupUtility()
         {
             FillBiomeLists(DefDatabase<BiomeDef>.AllDefsListForReading);
+            EditRockTerrain();
         }
 
         public static void FillBiomeLists(List<BiomeDef> biomeDefs)
@@ -37,6 +39,45 @@ namespace MorrowRim2
                     }
                 }
             }
+        }
+
+        /// <summary>
+        /// Edits specific details about implied terrain defs
+        /// </summary>
+        public static void EditRockTerrain()
+        {
+            foreach (ThingDef thingDef in from def in DefDatabase<ThingDef>.AllDefs
+                                          where RockProperties.Get(def) != null
+                                          select def)
+            {
+                RockProperties props = RockProperties.Get(thingDef);
+                if (props.roughTexPathReplacer != null && thingDef.building.naturalTerrain != null)
+                {
+                    EditImpliedTerrain(thingDef.building.naturalTerrain, props.roughTexPathReplacer, props.scatterTypeReplacer);
+                }
+                if (props.roughHewnTexPathReplacer != null && thingDef.building.leaveTerrain != null)
+                {
+                    EditImpliedTerrain(thingDef.building.leaveTerrain, props.roughHewnTexPathReplacer, props.scatterTypeReplacer);
+                }
+                if (props.smoothTexPathReplacer != null && thingDef.building.naturalTerrain.smoothedTerrain != null)
+                {
+                    EditImpliedTerrain(thingDef.building.naturalTerrain.smoothedTerrain, props.smoothTexPathReplacer, props.scatterTypeReplacer);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Need to reset graphic, and call postLoad, to update the terrain texture
+        /// </summary>
+        public static void EditImpliedTerrain(TerrainDef def, string newTexPath, string scatterType = null)
+        {
+            def.texturePath = newTexPath;
+            if (scatterType != null)
+            {
+                def.scatterType = scatterType;
+            }
+            def.graphic = BaseContent.BadGraphic;
+            def.PostLoad();
         }
     }
 }
