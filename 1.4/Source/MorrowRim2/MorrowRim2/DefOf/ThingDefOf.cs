2ebe1ea58a661aba4983db91303e366027c7edc0
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-04-16 15:01:26 +1200

    volcanic debris rain part 1
    TODO
    - custom on death item drop
    - possibly another retexture

---
+++ 1.4/Source/MorrowRim2/MorrowRim2/DefOf/ThingDefOf.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MorrowRim2
+{
+    [DefOf]
+    public static class ThingDefOf
+    {
+        static ThingDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(ThingDefOf));
+        }
+
+        public static ThingDef MorrowRim_VolcanicDebris;
+        public static ThingDef MorrowRim_VolcanicDebrisIncoming;
+    }
+}
---
+++ 1.4/Source/MorrowRim2/MorrowRim2/GameCondition/GameCondition_DebrisRain.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace MorrowRim2
+{
+    public class GameCondition_DebrisRain : GameCondition
+    {
+        public override void Init()
+        {
+            base.Init();
+			checkInterval = checkIntervalRange.RandomInRange;
+		}
+
+        public override void GameConditionTick()
+		{
+			List<Map> affectedMaps = AffectedMaps;
+			if (Find.TickManager.TicksGame % checkInterval == 0)
+			{
+				for (int i = 0; i < affectedMaps.Count; i++)
+				{
+					TryTriggerMeteor(affectedMaps[i]);
+				}
+			}
+		}
+
+		public void TryTriggerMeteor(Map map)
+		{
+			if (!TryFindCell(out IntVec3 intVec, map))
+			{
+				return;
+			}
+			SkyfallerMaker.SpawnSkyfaller(ThingDefOf.MorrowRim_VolcanicDebrisIncoming, ThingMaker.MakeThing(ThingDefOf.MorrowRim_VolcanicDebris), intVec, map);
+			checkInterval = checkIntervalRange.RandomInRange;
+		}
+
+		private bool TryFindCell(out IntVec3 cell, Map map)
+		{
+			int maxMineables = ThingSetMaker_Meteorite.MineablesCountRange.max;
+			return CellFinderLoose.TryFindSkyfallerCell(ThingDefOf.MorrowRim_VolcanicDebrisIncoming, map, out cell, 10, default, -1, true, false, false, false, true, true);
+		}
+
+		public int checkInterval;
+		public IntRange checkIntervalRange = new IntRange(341, 851);
+    }
+}
