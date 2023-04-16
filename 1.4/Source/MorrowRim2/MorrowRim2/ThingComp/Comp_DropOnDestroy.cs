992097010e69463133af4e8d5edb8cc8079d9f40
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-04-16 16:15:41 +1200

    CompProperties_DropOnDestroy

---
+++ 1.4/Source/MorrowRim2/MorrowRim2/ThingComp/Comp_DropOnDestroy.cs
@@ -0,0 +1,28 @@
+﻿using Verse;
+
+namespace MorrowRim2
+{
+    public class Comp_DropOnDestroy : ThingComp
+    {
+		public CompProperties_DropOnDestroy Props
+		{
+			get
+			{
+				return (CompProperties_DropOnDestroy)props;
+			}
+		}
+
+        public override void PostDestroy(DestroyMode mode, Map previousMap)
+        {
+            if (!Props.potentialDrops.NullOrEmpty())
+            {
+                PotentialDrops drop = Props.potentialDrops.RandomElementByWeight(x => x.weight);
+                Thing droppedThing = ThingMaker.MakeThing(drop.thingDef);
+                droppedThing.stackCount = drop.amountRange.RandomInRange;
+                GenPlace.TryPlaceThing(droppedThing, parent.Position, previousMap, ThingPlaceMode.Direct);
+            }
+
+            base.PostDestroy(mode, previousMap);
+        }
+    }
+}
