fd1902d9f16c3f2d802ec54762310d36b0eb24f7
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-04-17 14:42:43 +1200

    Initial ash terrain

--- 1.4/Source/MorrowRim2/MorrowRim2/DefModExtension/BiomeProperties.cs
+++ 1.4/Source/MorrowRim2/MorrowRim2/DefModExtension/BiomeProperties.cs
@@ -13,6 +13,7 @@ namespace MorrowRim2
         /* for biome gen */
         public bool disableBeaches = false;
         public List<TerrainReplacer> terrainReplacers;
+        public List<TerrainReplacer> roadReplacers;
         public ThingDef forcedRockType;
 
         /* general use */
--- 1.4/Source/MorrowRim2/MorrowRim2/Harmony/BiomeGen.cs
+++ 1.4/Source/MorrowRim2/MorrowRim2/Harmony/BiomeGen.cs
@@ -89,5 +89,29 @@ namespace MorrowRim2
                 }
             }
         }
+
+        /// <summary>
+        /// Replaces road terrain with different terrain.
+        /// </summary>
+        [HarmonyPatch(typeof(RoadDefGenStep_Place))]
+        [HarmonyPatch("Place")]
+        public static class RoadDefGenStep_Place_Place_Patch
+        {
+            [HarmonyPostfix]
+            public static void MorrowRim_RoadDefGenStep_Place_Patch(Map map, IntVec3 position)
+            {
+                BiomeProperties props = BiomeProperties.Get(map.Biome);
+                if (props != null && !props.roadReplacers.NullOrEmpty())
+                {
+                    foreach (TerrainReplacer replacer in props.roadReplacers)
+                    {
+                        if (replacer.originalTerrain == position.GetTerrain(map))
+                        {
+                            map.terrainGrid.SetTerrain(position, replacer.replacedTerrain);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
