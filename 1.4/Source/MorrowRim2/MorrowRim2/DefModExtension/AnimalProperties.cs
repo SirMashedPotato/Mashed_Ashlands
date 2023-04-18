d5efaaaeb91e16a75aad13c347a4fc336b0df5fb
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-04-18 17:17:23 +1200

    Animals specific landmarks patch

---
+++ 1.4/Source/MorrowRim2/MorrowRim2/DefModExtension/AnimalProperties.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace MorrowRim2
+{
+    public class AnimalProperties : DefModExtension
+    {
+        public bool requireRiver = false;
+        public bool requireCoast = false;
+        public bool requireCaves = false;
+        public bool requireHills = false;
+
+        public static AnimalProperties Get(Def def)
+        {
+            return def.GetModExtension<AnimalProperties>();
+        }
+    }
+}
