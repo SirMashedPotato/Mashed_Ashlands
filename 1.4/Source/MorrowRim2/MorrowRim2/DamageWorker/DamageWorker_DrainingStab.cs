133913e7cdf4a2dfb0bfb343595e0eef5e616f43
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-04-30 08:34:53 +1200

    draining stab

---
+++ 1.4/Source/MorrowRim2/MorrowRim2/DamageWorker/DamageWorker_DrainingStab.cs
@@ -0,0 +1,32 @@
+﻿using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace MorrowRim2
+{
+    public class DamageWorker_DrainingStab : DamageWorker_Stab
+    {
+        public override DamageResult Apply(DamageInfo dinfo, Thing thing)
+        {
+            if (thing is Pawn target && target.RaceProps.IsFlesh)
+            {
+                float num = 0.0230066683f;
+                num *= (dinfo.Amount * 0.3f);
+                if (num != 0f)
+                {
+                    float num2 = Mathf.Lerp(0.85f, 1.15f, Rand.ValueSeeded(target.thingIDNumber ^ 74374237));
+                    num *= num2;
+                    HealthUtility.AdjustSeverity(target, RimWorld.HediffDefOf.BloodLoss, num);
+
+                    Pawn pawn2 = dinfo.Instigator as Pawn;
+                    Need foodNeed = pawn2.needs.TryGetNeed(NeedDefOf.Food);
+                    if (foodNeed != null)
+                    {
+                        foodNeed.CurLevel += num;
+                    }
+                }
+            }
+            return base.Apply(dinfo, thing);
+        }
+    }
+}
