cb872395cd0a20f174e194890d81fc75f11de8a8
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-05-06 18:31:17 +1200

    thunderbug

---
+++ 1.4/Source/MorrowRim2/MorrowRim2/ThingComp/Comp_DischargeOnDamage.cs
@@ -0,0 +1,46 @@
+﻿using Verse;
+
+namespace MorrowRim2
+{
+    public class Comp_DischargeOnDamage : ThingComp
+    {
+        private CompProperties_DischargeOnDamage Props
+        {
+            get
+            {
+                return (CompProperties_DischargeOnDamage)props;
+            }
+        }
+
+        private int cooldownTicksLeft = 0;
+
+        public override void PostPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        {
+            Pawn p = parent as Pawn;
+            if (p.Spawned && !p.Dead)
+            {
+                if(cooldownTicksLeft <= 0)
+                {
+                    GenExplosion.DoExplosion(p.Position, p.Map, Props.radius, Props.damageDef, p, Props.damageAmount, -1);
+                    cooldownTicksLeft = Props.cooldownTicks;
+                }
+            }
+            base.PostPostApplyDamage(dinfo, totalDamageDealt);
+        }
+
+        public override void CompTick()
+        {
+            if (cooldownTicksLeft > 0)
+            {
+                cooldownTicksLeft--;
+            }
+            base.CompTick();
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref cooldownTicksLeft, "cooldownTicksLeft", 0);
+        }
+    }
+}
