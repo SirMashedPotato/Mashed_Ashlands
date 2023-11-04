101da7be8fde11e15a699a3db79cead2abab5221
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-11-05 10:12:19 +1300

    exploration mode support
    - Added a volcano map that reveals a single undiscovered volcano. Can be purchased rarely from any trader.
    - Moved the sound patch loadfolder to 1.4/

---
+++ 1.4/Source/Mashed_Ashlands/Mashed_Ashlands_ExplorationMode/CompUse/CompProperties_UseEffectVolcanoMap.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+
+namespace Mashed_Ashlands_ExplorationMode
+{
+    public class CompProperties_UseEffectVolcanoMap : CompProperties_UseEffect
+    {
+        public CompProperties_UseEffectVolcanoMap()
+        {
+            compClass = typeof(CompUseEffect_VolcanoMap);
+        }
+
+        public int size;
+        public int locations;
+    }
+}
