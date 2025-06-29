b663fd0737e3de8be1c8092ad0736d6fdc6f2a2a
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-06-30 10:40:59 +1200

    mudcrab merchant caravan encounter pt2

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/World/WorldObjectComp/Properties/WorldObjectCompProperties_MerchantEncounter.cs
@@ -0,0 +1,12 @@
+﻿using RimWorld;
+
+namespace Mashed_Ashlands
+{
+    public class WorldObjectCompProperties_MerchantEncounter : WorldObjectCompProperties
+    {
+        public WorldObjectCompProperties_MerchantEncounter() => compClass = typeof(WorldObjectComp_MerchantEncounter);
+
+        public IncidentDef incidentDef;
+        public float chance = 0.03f;
+    }
+}
---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/World/WorldObjectComp/WorldObjectComp_MerchantEncounter.cs
@@ -0,0 +1,55 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class WorldObjectComp_MerchantEncounter : WorldObjectComp
+    {
+        private const int tickInterval = GenDate.TicksPerDay / 2;
+        public WorldObjectCompProperties_MerchantEncounter Props => (WorldObjectCompProperties_MerchantEncounter)props;
+
+        public override void CompTickInterval(int delta)
+        {
+            base.CompTickInterval(delta);
+            if (parent.IsHashIntervalTick(tickInterval, delta))
+            {
+                if (parent is Caravan caravan)
+                {
+
+                    if (caravan.Faction == null || caravan.Faction != Faction.OfPlayer)
+                    {
+                        return;
+                    }
+
+                    if (caravan.Tile == null || caravan.Tile.Tile.PrimaryBiome == null)
+                    {
+                        return;
+                    }
+
+                    BiomeProperties props = BiomeProperties.Get(caravan.Tile.Tile.PrimaryBiome);
+                    if (props == null && !props.allowAshlandsIncidents)
+                    {
+                        return;
+                    }
+
+                    if (!Rand.Chance(Props.chance))
+                    {
+                        return;
+                    }
+
+                    GenerateIncident(caravan);
+                }
+            }
+        }
+
+        private void GenerateIncident(Caravan caravan)
+        {
+            IncidentParms incidentParms = StorytellerUtility.DefaultParmsNow(Props.incidentDef.category, caravan);
+            if (Props.incidentDef.Worker.CanFireNow(incidentParms))
+            {
+                Props.incidentDef.Worker.TryExecute(incidentParms);
+            }
+        }
+    }
+}
