c6c6d2d8093a2b69e8d77bb24c5ec137c42b78c3
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-07-14 12:35:58 +1200

    sulphuric springs + caves

--- 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/DefModExtension/BiomeProperties.cs
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/DefModExtension/BiomeProperties.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using RimWorld;
+using System.Collections.Generic;
 using System.Xml;
 using Verse;
 
@@ -17,21 +18,12 @@ namespace Mashed_Ashlands
         public List<TerrainReplacer> roadReplacers;
         public bool increasePollutedFertility = true;
         public bool restrictPlantsToTerrain = true;
+        public BiomeCaveProperties caveProps;
 
         /* general use */
         public List<TerrainReplacer> dryToReplacers;
         public bool allowAshlandsIncidents = true;
         public bool allowAshRaids = true;
-
-        /* for cave gen */
-        //public List<BiomePlantRecord> cavePlants;
-        //public bool forceCustomCavePlants = false;
-        //public bool increaseCavePlantWeight = true;
-        //public bool useCustomCaveTerrain = false;
-        //public TerrainDef otherTerrainDef;
-        //public TerrainDef waterTerrainDef;
-        //public float otherTerrainThreshold = 0.55f;
-        //public float waterThreshold = 0.93f;
 
         public static BiomeProperties Get(Def def) => def.GetModExtension<BiomeProperties>();
     }
@@ -46,5 +38,11 @@ namespace Mashed_Ashlands
             DirectXmlCrossRefLoader.RegisterObjectWantsCrossRef(this, "originalTerrain", xmlRoot);
             DirectXmlCrossRefLoader.RegisterObjectWantsCrossRef(this, "replacedTerrain", xmlRoot.InnerText);
         }
+    }
+
+    public class BiomeCaveProperties
+    {
+        public List<BiomePlantRecord> cavePlants;
+        public TerrainReplacer caveGravelReplacer;
     }
 }
---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/TileMutatorWorker/TileMutatorWorker_SulphuricCaves.cs
@@ -0,0 +1,62 @@
+﻿using RimWorld;
+using Verse;
+using Verse.Noise;
+
+namespace Mashed_Ashlands
+{
+    public class TileMutatorWorker_SulphuricCaves : TileMutatorWorker_Caves
+    {
+        private static readonly FloatRange LakeRadius = new FloatRange(10f, 15f);
+        private static readonly FloatRange LakeSquash = new FloatRange(1f, 1.25f);
+        private const float LakeNoiseFrequency = 0.03f;
+        private const float LakeNoiseStrength = 5f;
+        private const float LakeChancePerCell = 0.005f;
+        private const float WaterThreshold = 0.8f;
+        private const float SandThreshold = 0.5f;
+
+        public TileMutatorWorker_SulphuricCaves(TileMutatorDef def) : base(def)
+        {
+        }
+
+        public override void GeneratePostTerrain(Map map)
+        {
+            if (!ModsConfig.OdysseyActive)
+            {
+                return;
+            }
+            MapGenFloatGrid caves = MapGenerator.Caves;
+            base.GeneratePostTerrain(map);
+            foreach (IntVec3 allCell in map.AllCells)
+            {
+                if (caves[allCell] <= 0f || allCell.GetTerrain(map).IsWater || !Rand.Chance(LakeChancePerCell))
+                {
+                    continue;
+                }
+                ModuleBase input = new DistFromPoint(LakeRadius.RandomInRange);
+                input = new ScaleBias(-1.0, 1.0, input);
+                input = new Scale(LakeSquash.RandomInRange, 1.0, 1.0, input);
+                input = new Rotate(0.0, Rand.Range(0f, 360f), 0.0, input);
+                input = new Translate(-allCell.x, 0.0, -allCell.z, input);
+                input = MapNoiseUtility.AddDisplacementNoise(input, LakeNoiseFrequency, LakeNoiseStrength, 4, map.Tile.tileId);
+                CellRect cellRect = allCell.RectAbout(30, 30);
+                cellRect.ClipInsideMap(map);
+                foreach (IntVec3 item in cellRect)
+                {
+                    float num = (float)input.GetValue(item.x, 0.0, item.z);
+                    if (!(num < 0.5f))
+                    {
+                        item.GetEdifice(map)?.Destroy();
+                        if (num > WaterThreshold)
+                        {
+                            map.terrainGrid.SetTerrain(item, TerrainDefOf.Mashed_Ashlands_ShallowSulphuricWater);
+                        }
+                        else if (num > SandThreshold)
+                        {
+                            map.terrainGrid.SetTerrain(item, TerrainDefOf.Mashed_Ashlands_SulphuricSand);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
