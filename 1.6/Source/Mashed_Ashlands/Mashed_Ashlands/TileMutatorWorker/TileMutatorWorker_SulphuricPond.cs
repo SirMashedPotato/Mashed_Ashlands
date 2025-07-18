d39cda047f38aa22bdce0f22dd37644861125290
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-07-19 11:31:27 +1200

    extra ponds

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/TileMutatorWorker/TileMutatorWorker_SulphuricPond.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class TileMutatorWorker_SulphuricPond : TileMutatorWorker_Lake
+    {
+        protected override float LakeRadius => 0.3f;
+
+        public TileMutatorWorker_SulphuricPond(TileMutatorDef def) : base(def)
+        {
+        }
+
+        protected override void ProcessCell(IntVec3 cell, Map map)
+        {
+            float valAt = GetValAt(cell, map);
+            if (valAt > 0.5f)
+            {
+                map.terrainGrid.SetTerrain(cell, TerrainDefOf.Mashed_Ashlands_ShallowSulphuricWater);
+            }
+            else if (valAt > 0.45f && MapGenUtility.ShouldGenerateBeachSand(cell, map))
+            {
+                map.terrainGrid.SetTerrain(cell, TerrainDefOf.Mashed_Ashlands_SulphuricSand);
+            }
+        }
+    }
+}
---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/TileMutatorWorker/TileMutatorWorker_ThermalPond.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class TileMutatorWorker_ThermalPond : TileMutatorWorker_Lake
+    {
+        protected override float LakeRadius => 0.3f;
+
+        public TileMutatorWorker_ThermalPond(TileMutatorDef def) : base(def)
+        {
+        }
+
+        protected override void ProcessCell(IntVec3 cell, Map map)
+        {
+            float valAt = GetValAt(cell, map);
+            if (valAt > 0.5f)
+            {
+                map.terrainGrid.SetTerrain(cell, RimWorld.TerrainDefOf.HotSpring);
+            }
+            else if (valAt > 0.45f && MapGenUtility.ShouldGenerateBeachSand(cell, map))
+            {
+                map.terrainGrid.SetTerrain(cell, MapGenUtility.LakeshoreTerrainAt(cell, map));
+            }
+        }
+    }
+}
