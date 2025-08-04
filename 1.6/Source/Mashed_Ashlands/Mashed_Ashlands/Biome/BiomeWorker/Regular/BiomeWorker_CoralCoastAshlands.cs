344e4a58d696e704386cb0fe0f12662d8479f22d
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-08-04 14:18:03 +1200

    tube coral

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/Biome/BiomeWorker/Regular/BiomeWorker_CoralCoastAshlands.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld.Planet;
+using RimWorld;
+
+namespace Mashed_Ashlands
+{
+    public class BiomeWorker_CoralCoastAshlands : AshlandBiomeWorker
+    {
+        public override float GetScore_Main(BiomeDef biome, Tile tile, PlanetTile planetTile, WorldObject sourceObject)
+        {
+            if (!Mashed_Ashlands_ModSettings.EnableMeadowAshlands) //TODO
+            {
+                return -100f;
+            }
+            if (tile.PrimaryBiome != BiomeDefOf.Mashed_Ashlands_Ashlands)
+            {
+                return -100f;
+            }
+
+            return CoastBiomeWorker(biome, tile, planetTile, biome);
+        }
+    }
+}
