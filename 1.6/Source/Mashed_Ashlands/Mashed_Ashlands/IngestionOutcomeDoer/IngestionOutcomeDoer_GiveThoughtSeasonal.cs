6ce4f049dfe4d7a0c1b095e549b50409e23fecd0
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-10-22 11:14:58 +1300

    lava foot soup rework

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/IngestionOutcomeDoer/IngestionOutcomeDoer_GiveThoughtSeasonal.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class IngestionOutcomeDoer_GiveThoughtSeasonal : IngestionOutcomeDoer
+    {
+        public ThoughtDef thoughtDef;
+        public Quadrum quadrum = Quadrum.Undefined;
+
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            if (pawn.needs?.mood == null)
+            {
+                return;
+            }
+
+            if (GenDate.Quadrum(Find.TickManager.TicksAbs, Find.WorldGrid.LongLatOf(pawn.Tile).x) != quadrum)
+            {
+                return;
+            }
+
+            Thought_Memory thought_Memory = ThoughtMaker.MakeThought(thoughtDef, 0);
+            pawn.needs.mood.thoughts.memories.TryGainMemory(thought_Memory);
+        }
+    }
+}
