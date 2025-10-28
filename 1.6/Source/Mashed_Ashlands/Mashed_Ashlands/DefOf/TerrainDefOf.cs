91ab44b4be06127f0f766453854c6f4c2b58f89e
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-10-29 11:25:51 +1300

    sludge pond landmark/mutator

--- 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/DefOf/TerrainDefOf.cs
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/DefOf/TerrainDefOf.cs
@@ -14,5 +14,7 @@ namespace Mashed_Ashlands
         public static TerrainDef Mashed_Ashlands_SulphuricSand;
         public static TerrainDef Mashed_Ashlands_ShallowSulphuricWater;
         public static TerrainDef Mashed_Ashlands_DeepSulphuricWater;
+        public static TerrainDef Mashed_Ashlands_AshMudBlighted;
+        public static TerrainDef Mashed_Ashlands_BlightedSludge;
     }
 }
---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/TileMutatorWorker/TileMutatorWorker_SludgePond.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class TileMutatorWorker_SludgePond : TileMutatorWorker_Lake
+    {
+        protected override float LakeRadius => 0.3f;
+
+        public TileMutatorWorker_SludgePond(TileMutatorDef def) : base(def)
+        {
+        }
+
+        protected override void ProcessCell(IntVec3 cell, Map map)
+        {
+            float valAt = GetValAt(cell, map);
+            if (valAt > 0.5f)
+            {
+                map.terrainGrid.SetTerrain(cell, TerrainDefOf.Mashed_Ashlands_BlightedSludge);
+            }
+            else if (valAt > 0.45f && MapGenUtility.ShouldGenerateBeachSand(cell, map))
+            {
+                map.terrainGrid.SetTerrain(cell, TerrainDefOf.Mashed_Ashlands_AshMudBlighted);
+            }
+        }
+    }
+}
