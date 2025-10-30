a0e5a5572111a0b51048f821168dfa2580e91600
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-10-31 11:34:09 +1300

    retching netch

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/DeathActionWorker/DeathActionWorker_RotStinkCloud.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI.Group;
+
+namespace Mashed_Ashlands
+{
+    internal class DeathActionWorker_RotStinkCloud : DeathActionWorker
+    {
+        public override bool DangerousInMelee => true;
+
+        public override void PawnDied(Corpse corpse, Lord prevLord)
+        {
+            GenExplosion.DoExplosion(radius: (corpse.InnerPawn.ageTracker.CurLifeStageIndex == 0) ? 0.9f : ((corpse.InnerPawn.ageTracker.CurLifeStageIndex != 1) ? 2.9f : 1.9f), 
+                center: corpse.Position, map: corpse.Map, damType: DamageDefOf.Smoke, instigator: corpse.InnerPawn, damAmount: -1, postExplosionGasType: GasType.RotStink);
+        }
+    }
+}
