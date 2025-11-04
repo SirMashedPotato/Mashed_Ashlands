d524cf6110609ce3a1b8e30ed5c61a0da56a5e48
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-11-04 13:05:40 +1300

    kwama egg sculpture

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/ThingComp/Comp_RotstinkEmitter.cs
@@ -0,0 +1,30 @@
+﻿using Verse;
+using RimWorld;
+
+namespace Mashed_Ashlands
+{
+    public class Comp_RotstinkEmitter : ThingComp
+    {
+        public override void CompTickRare()
+        {
+            if (!parent.Spawned || parent.Map == null)
+            {
+                return;
+            }
+
+            if (!QualityUtility.TryGetQuality(parent, out QualityCategory quality))
+            {
+                return;
+            }
+
+            float chance = quality == QualityCategory.Awful ? 0.06f : quality == QualityCategory.Poor ? 0.03f : 0f;
+
+            if (!Rand.Chance(chance))
+            {
+                return;
+            }
+
+            GasUtility.AddGas(parent.Position, parent.Map, GasType.RotStink, 1f);
+        }
+    }
+}
