fa4488b63079500cd6eda98d1f46d6dd19477b71
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-11-12 14:23:33 +1300

    cave in generation and better mine structure

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/Biome/GenStep/GenStep_CaveIns.cs
@@ -0,0 +1,60 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class GenStep_CaveIns : GenStep
+    {
+        public override int SeedPart => 75889013;
+
+        public IntRange caveInCountRange;
+        public IntRange caveInSizeRange;
+        private const int maxTries = 600;
+
+        public override void Generate(Map map, GenStepParams parms)
+        {
+            int caveInTarget = caveInCountRange.RandomInRange;
+            int caveInCount = 0;
+            for (int i = 0; i < maxTries; i++)
+            {
+                int caveInSize = caveInSizeRange.RandomInRange;
+                CellFinder.TryFindRandomCell(map, c => CellValidator(map, c), out IntVec3 result);
+                if (result.IsValid)
+                {
+                    List<IntVec3> caveIn = GridShapeMaker.IrregularLump(result, map, caveInSize, c => CellValidator(map, c));
+                    foreach (IntVec3 cell in caveIn)
+                    {
+                        RoofCollapserImmediate.DropRoofInCells(cell, map);
+                    }
+                    if (caveInCount++ >= caveInTarget)
+                    {
+                        return;
+                    }
+                }
+            }
+            Log.Warning("GenStep_CaveIns hit max tries of " + maxTries);
+        }
+
+        private bool CellValidator(Map map, IntVec3 c)
+        {
+            if (c.DistanceToEdge(map) < 10)
+            {
+                return false;
+            }
+
+            if (!c.Standable(map))
+            {
+                return false;
+            }
+
+            c.TryGetFirstThing(map, out Thing thing);
+            if (thing != null && !(thing is Filth))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/TileMutatorWorker/TileMutatorWorker_UndergroundMine.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+
+namespace Mashed_Ashlands
+{
+    public class TileMutatorWorker_UndergroundMine : TileMutatorWorker_UndergroundCave
+    {
+        protected override float BranchChance => 0.1f;
+
+        protected override float MinTunnelWidth => 2.6f;
+
+        protected override float WidthOffsetPerCell => 0.0001f;
+
+        public TileMutatorWorker_UndergroundMine(TileMutatorDef def) : base(def)
+        {
+        }
+    }
+}
