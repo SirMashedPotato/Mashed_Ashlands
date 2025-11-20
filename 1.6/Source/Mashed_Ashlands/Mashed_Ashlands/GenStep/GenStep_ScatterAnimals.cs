e647520970c9ec380c81063bd95e1dae76a6e04d
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-11-20 14:53:10 +1300

    kwama in abandoned egg mine

--- 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/GenStep/GenStep_ScatterAnimals.cs
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/GenStep/GenStep_ScatterAnimals.cs
@@ -2,11 +2,11 @@
 
 namespace Mashed_Ashlands
 {
-    public class GenStep_KwamaWorkers : GenStep_ScatterThings
+    public class GenStep_ScatterAnimals : GenStep_ScatterThings
     {
         protected override void ScatterAt(IntVec3 loc, Map map, GenStepParams parms, int stackCount = 1)
         {
-            Pawn pawn = PawnGenerator.GeneratePawn(PawnKindDefOf.Mashed_Ashlands_KwamaWorker, null);
+            Pawn pawn = PawnGenerator.GeneratePawn(thingDef.race.AnyPawnKind, null);
             pawn.ageTracker.AgeBiologicalTicks = pawn.ageTracker.AdultMinAgeTicks;
             GenSpawn.Spawn(pawn, loc, map, Rot4.Random, WipeMode.FullRefund, false);
         }
