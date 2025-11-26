29a7f2a4d72167755f7751df055ebf83ebd26981
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-11-27 12:08:43 +1300

    strider crossing

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/DefOf/TileMutatorDefOf.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    [DefOf]
+    public static class TileMutatorDefOf
+    {
+        static TileMutatorDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(TileMutatorDefOf));
+        }
+
+        [MayRequireOdyssey]
+        public static TileMutatorDef Mashed_Ashlands_StriderCrossing;
+    }
+}
---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/IncidentWorker/IncidentWorker_SiltStriderCrossing.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class IncidentWorker_SiltStriderCrossing : IncidentWorker_SiltStriderPasses
+    {
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            if (parms.target is Map map)
+            {
+                if (map.TileInfo.mutatorsNullable.NullOrEmpty() || !map.TileInfo.Mutators.Contains(TileMutatorDefOf.Mashed_Ashlands_StriderCrossing))
+                {
+                    return false;
+                }
+
+                return base.CanFireNowSub(parms);
+            }
+            return false;
+        }
+    }
+}
