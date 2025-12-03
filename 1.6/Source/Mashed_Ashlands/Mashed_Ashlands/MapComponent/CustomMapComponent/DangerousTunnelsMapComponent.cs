852550fa56f98dcfa1d9c34c6c9c1c05fabc1074
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-12-03 13:08:17 +1300

    more re-organising of stuff

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/MapComponent/CustomMapComponent/DangerousTunnelsMapComponent.cs
@@ -0,0 +1,85 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public abstract class DangerousTunnelsMapComponent : CustomMapComponent
+    {
+        public PocketMapExit mineExit;
+        public Building_UndercaveEntrance mineEntrance;
+
+        private float instability;
+        private FloatRange instabilityGainRange = new FloatRange(0f, 0.06f);
+        private int checkInterval;
+        private IntRange checkIntervalRange = new IntRange(15000, 60000);
+
+        public Map SourceMap => (map.Parent as PocketMapParent)?.sourceMap;
+
+        public DangerousTunnelsMapComponent(Map map) : base(map)
+        {
+            instability = instabilityGainRange.RandomInRange;
+            checkInterval = checkIntervalRange.RandomInRange;
+        }
+
+        public override void MapGenerated()
+        {
+            base.MapGenerated();
+
+            mineEntrance = SourceMap?.listerThings?.ThingsOfDef(ThingDefOf.Mashed_Ashlands_UndercaveEntrance).FirstOrDefault() as Building_UndercaveEntrance;
+            if (mineEntrance == null)
+            {
+                Log.Warning("Abandoned mine entrance was not found after generating undercave, if this map was created via dev tools you can ignore this");
+                return;
+            }
+
+            mineExit = map.listerThings.ThingsOfDef(RimWorld.ThingDefOf.CaveExit).FirstOrDefault() as PocketMapExit;
+            if (mineExit == null)
+            {
+                Log.Error("Abandoned mine exit was not found after generating abandoned mine");
+                return;
+            }
+        }
+
+        public override void MapComponentTick()
+        {
+            base.MapComponentTick();
+            if (map == null)
+            {
+                return;
+            }
+
+            if (map.IsHashIntervalTick(checkInterval))
+            {
+                checkInterval = checkIntervalRange.RandomInRange;
+
+                if (instability >= 1f)
+                {
+                    TriggerInstabilityEffect();
+                    instability = 0f;
+                }
+                else
+                {
+                    if (Find.CurrentMap == map)
+                    {
+                        Find.CameraDriver.shaker.SetMinShake(1);
+                    }
+                    instability = Mathf.Clamp(instability + instabilityGainRange.RandomInRange, 0f, 1f);
+                }
+            }
+        }
+
+        public abstract void TriggerInstabilityEffect();
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref instability, "instability", 0f);
+            Scribe_Values.Look(ref checkInterval, "checkInterval", 60000);
+            Scribe_References.Look(ref mineEntrance, "mineEntrance");
+            Scribe_References.Look(ref mineExit, "mineExit");
+        }
+    }
+}
--- 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/MapComponent/CustomMapComponent/UnstableTunnelsMapComponent.cs
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/MapComponent/CustomMapComponent/UnstableTunnelsMapComponent.cs
@@ -1,83 +1,23 @@
 ﻿using RimWorld;
-using RimWorld.Planet;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
 using Verse;
 
 namespace Mashed_Ashlands
 {
-    public class UnstableTunnelsMapComponent : CustomMapComponent
+    public class UnstableTunnelsMapComponent : DangerousTunnelsMapComponent
     {
-        public PocketMapExit mineExit;
-        public Building_UndercaveEntrance mineEntrance;
-
         private static IntRange caveInCountRange = new IntRange(1,3);
         private static IntRange caveInSizeRange = new IntRange(10, 20);
 
-        private float instability;
-        private FloatRange instabilityGainRange = new FloatRange(0f, 0.06f);
-        private int checkInterval;
-        private IntRange checkIntervalRange = new IntRange(15000, 60000);
+        public UnstableTunnelsMapComponent(Map map) : base(map) 
+        { 
 
-        public Map SourceMap => (map.Parent as PocketMapParent)?.sourceMap;
-
-        public UnstableTunnelsMapComponent(Map map) : base(map)
-        {
-            instability = instabilityGainRange.RandomInRange;
-            checkInterval = checkIntervalRange.RandomInRange;
         }
 
-        public override void MapGenerated()
+        public override void TriggerInstabilityEffect()
         {
-            base.MapGenerated();
+            Messages.Message("Mashed_Ashlands_Undercave_CaveIn".Translate(mineEntrance.UndercaveTypeDef), mineExit, MessageTypeDefOf.NegativeEvent);
 
-            mineEntrance = SourceMap?.listerThings?.ThingsOfDef(ThingDefOf.Mashed_Ashlands_UndercaveEntrance).FirstOrDefault() as Building_UndercaveEntrance;
-            if (mineEntrance == null)
-            {
-                Log.Warning("Abandoned mine entrance was not found after generating undercave, if this map was created via dev tools you can ignore this");
-                return;
-            }
-
-            mineExit = map.listerThings.ThingsOfDef(RimWorld.ThingDefOf.CaveExit).FirstOrDefault() as PocketMapExit;
-            if (mineExit == null)
-            {
-                Log.Error("Abandoned mine exit was not found after generating abandoned mine");
-                return;
-            }
-        }
-
-        public override void MapComponentTick()
-        {
-            base.MapComponentTick();
-            if (map == null)
-            {
-                return;
-            }
-
-            if (map.IsHashIntervalTick(checkInterval))
-            {
-                checkInterval = checkIntervalRange.RandomInRange;
-
-                if (instability >= 1f)
-                {
-                    Messages.Message("Mashed_Ashlands_Undercave_CaveIn".Translate(mineEntrance.UndercaveTypeDef), mineExit, MessageTypeDefOf.NegativeEvent);
-                    TriggerCaveIn();
-                    instability = 0f;
-                }
-                else
-                {
-                    if (Find.CurrentMap == map)
-                    {
-                        Find.CameraDriver.shaker.SetMinShake(1);
-                    }
-                    instability = Mathf.Clamp(instability + instabilityGainRange.RandomInRange, 0f, 1f);
-                }
-            }
-        }
-
-        private void TriggerCaveIn()
-        {
             int caveInTarget = caveInCountRange.RandomInRange;
             for (int i = 0; i < caveInTarget; i++)
             {
@@ -92,15 +32,6 @@ namespace Mashed_Ashlands
                     }
                 }
             }
-        }
-
-        public override void ExposeData()
-        {
-            base.ExposeData();
-            Scribe_Values.Look(ref instability, "instability", 0f);
-            Scribe_Values.Look(ref checkInterval, "checkInterval", 60000);
-            Scribe_References.Look(ref mineEntrance, "mineEntrance");
-            Scribe_References.Look(ref mineExit, "mineExit");
         }
     }
 }
