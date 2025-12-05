82d7ec8ac5a83e3460aa86b08913397a5493eaf3
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-12-05 14:15:49 +1300

    improved cave in generation again

--- 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/GenStep/GenStep_CaveIns.cs
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/GenStep/GenStep_CaveIns.cs
@@ -10,10 +10,15 @@ namespace Mashed_Ashlands
 
         public IntRange caveInCountRange;
         public IntRange caveInSizeRange;
+        public int minSpacing = 0;
         private const int maxTries = 600;
+
+        [Unsaved(false)]
+        protected List<IntVec3> usedSpots = new List<IntVec3>();
 
         public override void Generate(Map map, GenStepParams parms)
         {
+            usedSpots.Clear();
             int caveInTarget = caveInCountRange.RandomInRange;
             int caveInCount = 0;
             for (int i = 0; i < maxTries; i++)
@@ -26,6 +31,7 @@ namespace Mashed_Ashlands
                     foreach (IntVec3 cell in caveIn)
                     {
                         RoofCollapserImmediate.DropRoofInCells(cell, map);
+                        usedSpots.Add(cell);
                     }
                     if (caveInCount++ >= caveInTarget)
                     {
@@ -36,14 +42,31 @@ namespace Mashed_Ashlands
             Log.Warning("GenStep_CaveIns hit max tries of " + maxTries);
         }
 
-        public bool CellValidator(Map map, IntVec3 c)
+        protected bool CellValidator(Map map, IntVec3 c)
         {
             if (c.DistanceToEdge(map) < 10)
             {
                 return false;
             }
 
+            if (minSpacing > 0 && NearUsedSpot(c))
+            {
+                return false;
+            }
+
            return Utility.CaveInCellValidator(map, c);
+        }
+
+        protected bool NearUsedSpot(IntVec3 c)
+        {
+            for (int i = 0; i < usedSpots.Count; i++)
+            {
+                if ((usedSpots[i] - c).LengthHorizontalSquared <= minSpacing * minSpacing)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
