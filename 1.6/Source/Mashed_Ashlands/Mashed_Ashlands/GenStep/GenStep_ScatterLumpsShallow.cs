6dbe7782c5665e7a800d37ba21016d43598c433a
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-12-05 13:37:01 +1300

    reworking lump spawning

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/GenStep/GenStep_ScatterLumpsShallow.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class GenStep_ScatterLumpsShallow : GenStep_ScatterLumpsMineable
+    {
+        protected override bool CanScatterAt(IntVec3 c, Map map)
+        {
+            if (base.CanScatterAt(c, map))
+            {
+                foreach (IntVec3 neighbourCell in GenAdj.CellsAdjacentCardinal(c, Rot4.North, IntVec2.One))
+                {
+                    if (neighbourCell.InBounds(map))
+                    {
+                        if (neighbourCell.GetEdifice(map) == null)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
