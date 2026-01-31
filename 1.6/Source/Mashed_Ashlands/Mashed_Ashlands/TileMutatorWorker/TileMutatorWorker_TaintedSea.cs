ea8d1a1c46f40e4d71a5bdbefe47a4560f7a8512
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2026-01-31 17:16:16 +1300

    Tainted sea mutator

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/TileMutatorWorker/TileMutatorWorker_TaintedSea.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class TileMutatorWorker_TaintedSea : TileMutatorWorker
+    {
+        public TileMutatorWorker_TaintedSea(TileMutatorDef def) : base(def)
+        {
+        }
+
+        public override void GeneratePostTerrain(Map map)
+        {
+            if (ModsConfig.OdysseyActive)
+            {
+                foreach(IntVec3 cell in map.AllCells)
+                {
+                    TerrainDef terrain = cell.GetTerrain(map);
+                    if (terrain.IsWater)
+                    {
+                        if (terrain == map.BiomeAt(cell).oceanShallowTerrain || terrain == RimWorld.TerrainDefOf.WaterOceanShallow)
+                        {
+                            map.terrainGrid.SetTerrain(cell, TerrainDefOf.Mashed_Ashlands_WaterShallowTaintedOcean);
+                        }
+                        else
+                        {
+                            if (terrain == map.BiomeAt(cell).oceanDeepTerrain || terrain == RimWorld.TerrainDefOf.WaterOceanDeep)
+                            {
+                                map.terrainGrid.SetTerrain(cell, TerrainDefOf.Mashed_Ashlands_WaterDeepTaintedOcean);
+                            }
+                        }
+                    }
+                }
+            }
+            base.GeneratePostTerrain(map);
+        }
+    }
+}
