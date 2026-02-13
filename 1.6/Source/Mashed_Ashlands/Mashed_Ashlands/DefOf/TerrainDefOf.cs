ae6babd6abff1437ca41a1028f2345a4d652d4f1
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2026-02-13 14:52:30 +1300

    dry lake landmarks and mutators

--- 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/DefOf/TerrainDefOf.cs
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/DefOf/TerrainDefOf.cs
@@ -11,13 +11,22 @@ namespace Mashed_Ashlands
             DefOfHelper.EnsureInitializedInCtor(typeof(TerrainDefOf));
         }
 
+        public static TerrainDef Mashed_Ashlands_CrackedAshSoil;
+        public static TerrainDef Mashed_Ashlands_AshSand;
+        public static TerrainDef Mashed_Ashlands_CrackedAshSoilBlighted;
+        public static TerrainDef Mashed_Ashlands_AshSandBlighted;
+        public static TerrainDef Mashed_Ashlands_CrackedAshSoilVolcanic;
+        public static TerrainDef Mashed_Ashlands_AshSandVolcanic;
+
         public static TerrainDef Mashed_Ashlands_SulphuricSand;
         public static TerrainDef Mashed_Ashlands_ShallowSulphuricWater;
         public static TerrainDef Mashed_Ashlands_DeepSulphuricWater;
+
         public static TerrainDef Mashed_Ashlands_AshMudBlighted;
         public static TerrainDef Mashed_Ashlands_BlightedSludge;
         public static TerrainDef Mashed_Ashlands_AshMud;
         public static TerrainDef Mashed_Ashlands_ThermalMud;
+
         [MayRequireOdyssey]
         public static TerrainDef Mashed_Ashlands_WaterShallowTaintedOcean;
         [MayRequireOdyssey]
