31792170aeca50da5b78db103b9d26154c42b81d
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2026-02-18 13:18:40 +1300

    sulphuric lake mutator/landmark

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/TileMutatorWorker/Lake/TileMutatorWorker_SulphuricLake.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class TileMutatorWorker_SulphuricLake : TileMutatorWorker_Lake
+    {
+        public TileMutatorWorker_SulphuricLake(TileMutatorDef def) : base(def)
+        {
+        }
+
+        protected override void ProcessCell(IntVec3 cell, Map map)
+        {
+            float valAt = GetValAt(cell, map);
+            if (GenerateDeepWater && valAt > 0.75f)
+            {
+                map.terrainGrid.SetTerrain(cell, TerrainDefOf.Mashed_Ashlands_DeepSulphuricWater);
+            }
+            else if (valAt > 0.5f)
+            {
+                map.terrainGrid.SetTerrain(cell, TerrainDefOf.Mashed_Ashlands_ShallowSulphuricWater);
+            }
+            else if (valAt > 0.45f)
+            {
+                map.terrainGrid.SetTerrain(cell, TerrainDefOf.Mashed_Ashlands_SulphuricSand);
+            }
+        }
+    }
+}
