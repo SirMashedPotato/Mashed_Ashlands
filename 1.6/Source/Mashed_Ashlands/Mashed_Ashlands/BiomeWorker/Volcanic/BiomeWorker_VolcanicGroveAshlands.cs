715bf94699ac8ac62e2ab6608969f0eb5935db7a
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2026-02-25 10:48:54 +1300

    a thing for later

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/BiomeWorker/Volcanic/BiomeWorker_VolcanicGroveAshlands.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+
+namespace Mashed_Ashlands
+{
+    public class BiomeWorker_VolcanicGroveAshlands : AshlandBiomeWorker
+    {
+        public override float GetScore_Main(BiomeDef biome, Tile tile, PlanetTile planetTile, WorldObject sourceObject = null)
+        {
+            if (!Mashed_Ashlands_ModSettings.EnableVolcanicBadlandsAshlands) //todo
+            {
+                return -100f;
+            }
+            if (tile.PrimaryBiome != BiomeDefOf.Mashed_Ashlands_VolcanicAshlands)
+            {
+                return -100f;
+            }
+
+            return JungleBiomeWorker(biome, tile, planetTile);
+        }
+    }
+}
