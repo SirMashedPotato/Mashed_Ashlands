400c465eb1c008cbb478064b9ee9878a5fc6c037
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2026-04-02 11:24:15 +1300

    Scamp merchant

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/CompUse/CompUseEffect_SpawnPawn.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class CompUseEffect_SpawnPawn : CompUseEffect
+    {
+        public CompProperties_UseEffectSpawnPawn Props => (CompProperties_UseEffectSpawnPawn)props;
+
+        public override void PrepareTick()
+        {
+        }
+
+        public override void DoEffect(Pawn usedBy)
+        {
+            Pawn pawn = PawnGenerator.GeneratePawn(Props.pawnKindDef, usedBy.Faction ?? null);
+            if (Rand.Chance(0.06f))
+            {
+                pawn.health.AddHediff(HediffDefOf.AlcoholAddiction);
+            }
+            GenSpawn.Spawn(pawn, parent.Position, usedBy.Map, Rot4.Random, WipeMode.Vanish, false);
+            Messages.Message("Mashed_Ashlands_SummonedDaedra".Translate(parent,usedBy, pawn), pawn, MessageTypeDefOf.NeutralEvent, false);
+        }
+    }
+}
---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/CompUse/Properties/CompProperties_UseEffectSpawnPawn.cs
@@ -0,0 +1,11 @@
+﻿using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class CompProperties_UseEffectSpawnPawn : CompProperties
+    {
+        public CompProperties_UseEffectSpawnPawn() => compClass = typeof(CompUseEffect_SpawnPawn);
+
+        public PawnKindDef pawnKindDef;
+    }
+}
