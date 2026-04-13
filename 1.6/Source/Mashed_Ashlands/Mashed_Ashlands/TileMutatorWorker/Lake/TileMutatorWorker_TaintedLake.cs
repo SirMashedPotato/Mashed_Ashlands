7c2a714556edc1f5d34f945eb064ff1e5c8faac7
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2026-04-14 08:56:27 +1200

    tainted lake mutator/landmark

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/TileMutatorWorker/Lake/TileMutatorWorker_TaintedLake.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class TileMutatorWorker_TaintedLake : TileMutatorWorker_Lake
+    {
+        public TileMutatorWorker_TaintedLake(TileMutatorDef def) : base(def)
+        {
+        }
+
+        protected override void ProcessCell(IntVec3 cell, Map map)
+        {
+            float valAt = GetValAt(cell, map);
+            if (GenerateDeepWater && valAt > 0.75f)
+            {
+                map.terrainGrid.SetTerrain(cell, TerrainDefOf.Mashed_Ashlands_WaterDeepTaintedOcean);
+            }
+            else if (valAt > 0.5f)
+            {
+                map.terrainGrid.SetTerrain(cell, TerrainDefOf.Mashed_Ashlands_WaterShallowTaintedOcean);
+            }
+            else if (valAt > 0.45f)
+            {
+                map.terrainGrid.SetTerrain(cell, map.BiomeAt(cell).lakeBeachTerrain);
+            }
+        }
+    }
+}
