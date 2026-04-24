f979023d28d9d6a412e42aeb90072f8d9704623c
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2026-04-24 16:28:28 +1200

    new sulphuric rain overlay

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/Weather/WeatherOverlay/WeatherOverlay_SulphuricRain.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    [StaticConstructorOnStartup]
+    public class WeatherOverlay_SulphuricRain : WeatherOverlayDualPanner
+    {
+        private static readonly Material OverlayWorld = MatLoader.LoadMat("Weather/RainOverlayWorld");
+        public static readonly Color overlayColour = new Color(0.9f, 1f, 0.7f, 0.8f);
+
+        public WeatherOverlay_SulphuricRain()
+        {
+            worldOverlayMat = OverlayWorld;
+            worldOverlayPanSpeed1 = 0.015f;
+            worldPanDir1 = new Vector2(-0.25f, -1f);
+            worldPanDir1.Normalize();
+            worldOverlayPanSpeed2 = 0.022f;
+            worldPanDir2 = new Vector2(-0.24f, -1f);
+            worldPanDir2.Normalize();
+        }
+
+        public override void SetOverlayColor(Color color)
+        {
+            OverlayWorld.color = overlayColour;
+        }
+    }
+}
