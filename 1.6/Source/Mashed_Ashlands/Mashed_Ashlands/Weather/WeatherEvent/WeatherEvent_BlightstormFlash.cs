4a10088d676765112f7965b35835f403de777780
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2026-04-30 10:34:32 +1200

    WeatherEvent_BlightstormFlash

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/Weather/WeatherEvent/WeatherEvent_BlightstormFlash.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Mashed_Ashlands
+{
+    public class WeatherEvent_BlightstormFlash : WeatherEvent_LightningFlash
+    {
+        private static readonly SkyColorSet BlightstormFlashColors = new SkyColorSet(
+            new Color(1f, 0.85f, 0.8f), 
+            new Color(0.85f, 0.7f, 0.7f), 
+            new Color(1f, 0.95f, 0.9f), 1.15f);
+        public override SkyTarget SkyTarget => new SkyTarget(1f, BlightstormFlashColors, 1f, 1f);
+
+        public WeatherEvent_BlightstormFlash(Map map) : base(map)
+        {
+            duration = Rand.Range(15, 60);
+            shadowVector = new Vector2(Rand.Range(-5f, 5f), Rand.Range(-5f, 0f));
+        }
+    }
+}
