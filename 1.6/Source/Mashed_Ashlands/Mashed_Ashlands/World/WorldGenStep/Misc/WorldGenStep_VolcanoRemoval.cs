45aa7e5099ab65b1b76e35163142a30da76fef1e
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2026-08-19 12:53:48 +1200

    WorldGenStep_VolcanoRemoval  class

---
+++ 1.6/Source/Mashed_Ashlands/Mashed_Ashlands/World/WorldGenStep/Misc/WorldGenStep_VolcanoRemoval.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld.Planet;
+using Verse;
+using System.Collections.Generic;
+
+namespace Mashed_Ashlands
+{
+    public class WorldGenStep_VolcanoRemoval : WorldGenStep
+    {
+        public override int SeedPart => 0;
+
+        public override void GenerateFresh(string seed, PlanetLayer layer)
+        {
+            List<WorldObject> volcanos = WorldGenUtility.GetWorldVolcanosForLayer(layer);
+            foreach (WorldObject volcano in volcanos)
+            {
+                volcano.Destroy();
+            }
+        }
+    }
+}
